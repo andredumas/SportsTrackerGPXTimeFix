919ea1c57f458a74a570784b620d7732aa819b57
André Dumas <agdumas@gmail.com>
2012-08-04 20:33:01 +1000

    Date conversion working

--- src/SportsTrackerGPXTimeFix.scala
+++ src/SportsTrackerGPXTimeFix.scala
@@ -2,20 +2,30 @@
 
 
 
-import java.text.DateFormat
-import javax.xml.bind.DatatypeConverter
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.TimeZone
 
-object SportsTrackerGPXTimeFix extends App {
-	println("Hello World")
-	
-	// Find file list, process each file
+import javax.xml.bind.DatatypeConverter
+
+object SportsTrackerGPXTimeFix {
+  val format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S")
+  
+  def convert(date: String) : String = {
+	  val calendar = DatatypeConverter.parseDateTime(date) 
+	  calendar.setTimeZone(TimeZone.getTimeZone("UTC"))
+	  
+	  val timezeonConvertedDate = format.format(calendar.getTime())
+	  println(date + " -> " + timezeonConvertedDate)
+	  return timezeonConvertedDate
+  }
+  
+  def main(args: Array[String]) {
+	  // Find file list, process each file
 //		x match {
 //	  case regex.matches => // Convert timezone and pass through
 //	  case _ => // pass through all lines
 //	}
-//	println(DateFormat.getInstance().parse("2012-04-02T08:12:15.14"))
-	println(DatatypeConverter.parseDateTime("2012-04-02T08:12:15.14").getTime())
-	println(DatatypeConverter.parseDateTime("2012-03-31T08:12:15.14").getTime())
-	val calendar = DatatypeConverter.parseDateTime("2012-03-31T08:12:15.14") 
+	  convert("2012-04-02T08:12:15.14")
+	  convert("2012-03-31T08:12:15.14")
+  }
 }
