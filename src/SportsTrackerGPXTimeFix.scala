9eb23d98bf256184dc2c10c7e08d9c0512550dff
André Dumas <agdumas@gmail.com>
2012-08-05 19:30:56 +1000

    Added some sample files, adjusted app

--- src/SportsTrackerGPXTimeFix.scala
+++ src/SportsTrackerGPXTimeFix.scala
@@ -12,7 +12,7 @@ import javax.xml.bind.DatatypeConverter
 
 object SportsTrackerGPXTimeFix {
   val Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
-  val TimeLine = """(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{1,3})""".r
+  val TimeLine = """(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{1,3})?)""".r
   
   def convert(date: String) : String = {
 	  val calendar = DatatypeConverter.parseDateTime(date) 
@@ -22,17 +22,15 @@ object SportsTrackerGPXTimeFix {
 	  return timezeonConvertedDate
   }
   
-  def convertTimeLine(timeLine: String): String = {
-    TimeLine.replaceAllIn(timeLine, (m: Match) => 
-      convert(m.group(1)) + "Z"
-    )
-   }
+  def convertTimeLine(timeLine: String): String = TimeLine.replaceAllIn(timeLine, (m: Match) => convert(m.group(1)) + "Z")
   
   def main(args: Array[String]) {
-	  if(args.length < 1)
-	    throw new IllegalArgumentException("Need to specify the file to process as an argument")
+	  if(args.length < 1) {
+	    println("Need to specify the file to process as an argument")
+	    return
+	  }
 	  
-	  Source.fromInputStream(this.getClass().getResourceAsStream(args(0))).getLines().foreach { line: String =>
+	  Source.fromFile(args(0)).getLines().foreach { line: String =>
 	    println(convertTimeLine(line))
 	  }
   }
