964ca4830941e18f1e4ba6482cadfe036334b3ba
andredumas <agdumas@gmail.com>
2012-08-06 16:54:01 +1000

    Was adjusting time the wrong way. A few other cleanups

--- src/SportsTrackerGPXTimeFix.scala
+++ src/SportsTrackerGPXTimeFix.scala
@@ -7,18 +7,17 @@ import scala.util.matching.Regex.Match
 import javax.xml.bind.DatatypeConverter
 
 object SportsTrackerGPXTimeFix {
-  val Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
-  val TimeLine = """(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{1,3})?)""".r
+  val timeRegex = """(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{1,3})?)""".r
+  val iso8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+  // Always normalising to UTC
+  iso8601Format.setTimeZone(TimeZone.getTimeZone("UTC"))
   
-  def convert(date: String) : String = {
+  def convertToUTC(date: String) : String = {
 	  val calendar = DatatypeConverter.parseDateTime(date) 
-	  calendar.setTimeZone(TimeZone.getTimeZone("UTC"))
-	  
-	  val timezeonConvertedDate = Format.format(calendar.getTime())
-	  return timezeonConvertedDate
+	  return iso8601Format.format(calendar.getTime())
   }
   
-  def convertTimeLine(timeLine: String): String = TimeLine.replaceAllIn(timeLine, (m: Match) => convert(m.group(1)) + "Z")
+  def convertTimeLine(timeLine: String): String = timeRegex.replaceAllIn(timeLine, (m: Match) => convertToUTC(m.group(1)) + "Z")
   
   def main(args: Array[String]) {
 	  if(args.length < 1) {
